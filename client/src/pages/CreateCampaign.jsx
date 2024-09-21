44835672894c87abb23d7083aa76de44cd2308b8
Kanishk Chaudhary <opkanix@gmail.com>
2024-09-21 23:37:13 +0530

    "Added imports and initialized state variables in CreateCampaign component"

--- client/src/pages/CreateCampaign.jsx
+++ client/src/pages/CreateCampaign.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react';
+import {useNavigate} from 'react-router-dom';
+import {ethers} from 'ethers';
+import {money} from '../assets';
+import {CustomButton} from '../components';
+import { checkIfImage} from '../utils';
 
 const CreateCampaign = () => {
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(flase);
+  const [form, setForm] = useState({
+
+  });
   return (
-    <div>CreateCampaign</div>
+    <div className=''>
+      
+    </div>
   )
 }
 
