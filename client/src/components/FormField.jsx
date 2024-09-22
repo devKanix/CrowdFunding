07c5b0ca84e0e9bc7c38ce013bdf4d8a8c1e787b
Kanishk Chaudhary <opkanix@gmail.com>
2024-09-22 20:27:45 +0530

    "Added onChange prop to FormField component and updated CreateCampaign page to use it."

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const FormField = ({ labelName, placeholder, inputType, isTextArea, value }) => {
+const FormField = ({ labelName, placeholder, inputType, isTextArea, value, onChange }) => {
   return (
     <label className='flex-1 w-full flex flex-col'>
       {labelName && (
@@ -12,6 +12,7 @@ const FormField = ({ labelName, placeholder, inputType, isTextArea, value }) => 
         <textarea
           required
           value={value}
+          onChange={onChange}
           rows={10}
           placeholder={placeholder}
           className='py-[15px] sm:px-[25px] px-[15px] outline-none border-[1px] border-[#3a3a43] bg-transparent font-epilogue text-white text-[14px] placeholder:text-[#4b5264] rounded-[10px] sm:min-w-[300px]'
@@ -20,6 +21,7 @@ const FormField = ({ labelName, placeholder, inputType, isTextArea, value }) => 
         <input
           required
           value={value}
+          onChange={onChange}
           type={inputType}
           stp="0.1"
           placeholder={placeholder}
